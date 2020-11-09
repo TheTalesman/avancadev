e033f47fc70f15106a442bd29a2398c439fea6ef
Alison Vargas <thetalesman@localhost.localdomain>
2020-11-09 13:59:24 -0300

    Dockerfile para todos servicos e docker-compose subindo a app inteira

--- filas-aula-2/b/b.go
+++ filas-aula-2/b/b.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/joho/godotenv"
-	uuid "github.com/satori/go.uuid"
-	"github.com/streadway/amqp"
-	"github.com/wesleywillians/go-rabbitmq/queue"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+
+	"github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
+	"github.com/streadway/amqp"
+	"github.com/wesleywillians/go-rabbitmq/queue"
 )
 
 type Result struct {
@@ -23,7 +24,8 @@ type Order struct {
 }
 
 func NewOrder() Order {
-	return Order{ID: uuid.NewV4()}
+	id, _ := uuid.NewV4()
+	return Order{ID: id}
 }
 
 const (
@@ -58,7 +60,7 @@ func process(msg amqp.Delivery) {
 	order := NewOrder()
 	json.Unmarshal(msg.Body, &order)
 
-	resultCoupon := makeHttpCall("http://localhost:9092", order.Coupon)
+	resultCoupon := makeHttpCall("http://localhost:9091", order.Coupon)
 
 	switch resultCoupon.Status {
 	case InvalidCoupon:
